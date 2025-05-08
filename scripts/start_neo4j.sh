63d389efcda2d610c8da44f6cb3b3a2ef5fc48a1
Andy <andy@agiledimensions.com.au>
2025-05-08 17:10:40 +0800

    Updated start and stop scripts for Neo4j location change

--- scripts/start_neo4j.sh
+++ scripts/start_neo4j.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
 # Start Neo4j server
-# Check for environment variables first
-if [ -n "$NEO4J_HOME" ]; then
-    echo "Using Neo4j installation from NEO4J_HOME: $NEO4J_HOME"
-    "$NEO4J_HOME/bin/neo4j" console
-elif [ -d "$HOME/.local/neo4j" ]; then
-    echo "Using Neo4j installation from ~/.local/neo4j"
-    "$HOME/.local/neo4j/bin/neo4j" console
-elif [ -d "$(dirname "$0")/../neo4j" ]; then
-    echo "Using Neo4j installation from project directory"
-    cd "$(dirname "$0")/.."
-    ./neo4j/bin/neo4j console
-else
-    echo "❌ Neo4j installation not found!"
-    echo "Please install Neo4j using: ./scripts/install_neo4j.sh"
-    exit 1
-fi
+"/Users/andyspamer/.local/neo4j/bin/neo4j" console
